a6ea4c48ba7266bd30c44c5b1602b726696b49c2
walnutsv6 <miyahara.tomofumi@gmail.com>
2026-02-26 16:17:04 +0900

    ESLint Install(Incomplete)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { JSX } from 'react';
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+const App = (): JSX.Element => {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +17,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+export default App
