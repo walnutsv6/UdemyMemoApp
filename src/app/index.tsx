ded72b4491d759cc4cf35cc2caf25d974c717d31
walnutsv6 <miyahara.tomofumi@gmail.com>
2026-02-27 12:44:05 +0900

    Markup memo list screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,58 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { JSX } from 'react'
 
 const Index = (): JSX.Element => {
     return (
-        <View>
-            <Text>Hello, World!</Text>
+        <View style={styles.container}>
+            <View>
+                {/* ヘッダー */}
+                <View>
+                    <Text>Memo App</Text>
+                    <Text>ログアウト</Text>
+                </View>
+            </View>
+            <View>
+                {/* メモリスト */}
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>2023年10月1日 10:00</Text>
+                </View>
+                <View>
+                    <Text>X</Text>
+                </View>
+
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>2023年10月1日 10:00</Text>
+                </View>
+                <View>
+                    <Text>X</Text>
+                </View>
+
+                <View>
+                    <Text>買い物リスト</Text>
+                    <Text>2023年10月1日 10:00</Text>
+                </View>
+                <View>
+                    <Text>X</Text>
+                </View>
+
+            </View>
+
+            {/* 追加ボタン */}
+            <View>
+                <Text>＋</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default Index
