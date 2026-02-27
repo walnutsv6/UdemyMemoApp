88f666110f19e5fb05ed895e721b9a5d55369b79
Tomofumi Miyahara <miyahara@lyzon.co.jp>
2026-02-27 18:51:09 +0900

    add Login screen

---
+++ src/app/memo/create.tsx
@@ -0,0 +1,41 @@
+import { View, TextInput, StyleSheet, KeyboardAvoidingView } from 'react-native'
+import { JSX } from 'react'
+
+import Header from '../../components/Header'
+import CircleButton from '../../components/CircleButton'
+import Icon from '../../components/icon'
+
+const Create = (): JSX.Element => {
+    return (
+        <KeyboardAvoidingView style={styles.container} behavior='height'>
+            <Header />
+            <View style={styles.InputContainer}>
+                <TextInput style={styles.input} value="" multiline />
+            </View>
+            <CircleButton>
+                <Icon name="check" size={40} color="#ffffff" />
+            </CircleButton>
+        </KeyboardAvoidingView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF'
+    },
+    InputContainer: {
+        paddingVertical: 32,
+        paddingHorizontal: 27,
+        flex: 1
+    },
+    input: {
+        flex: 1,
+        fontSize: 16,
+        lineHeight: 24,
+        // Androidでは反映されるが、iOSでは反映されない
+        textAlignVertical: 'top'
+    }
+})
+
+export default Create
