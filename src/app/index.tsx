a996f8f924b5a4528fac151d5b3b8c39c4fd2c76
Tomofumi Miyahara <miyahara@lyzon.co.jp>
2026-02-27 18:35:53 +0900

    add Editing Memo screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Redirect } from "expo-router"
 import {JSX} from "react"
 
 const Index = (): JSX.Element => {
-    return <Redirect href="/memo/detail" />
+    return <Redirect href="/memo/edit" />
 }
 
 export default Index
