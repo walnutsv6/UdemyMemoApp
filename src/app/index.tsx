e1684900870643706a3a098f25571e1f86cd7e99
Tomofumi Miyahara <miyahara@lyzon.co.jp>
2026-03-04 16:58:16 +0900

    add login state

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,16 @@
-import { Redirect } from "expo-router"
-import {JSX} from "react"
+import { Redirect, router } from "expo-router"
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../config'
+import { JSX, useEffect } from "react"
 
 const Index = (): JSX.Element => {
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user !== null) {
+                router.replace('/memo/list')
+            }
+        })
+    }, [])
     return <Redirect href="/auth/log_in" />
 }
 
