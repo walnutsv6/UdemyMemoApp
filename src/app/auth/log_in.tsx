21ab19be11adc89d37fe68f4eb09020008c2d07c
Tomofumi Miyahara <miyahara@lyzon.co.jp>
2026-03-04 11:31:43 +0900

    optimize Email and Password input

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,8 +1,7 @@
 import {
     View, Text, TextInput,
     StyleSheet, TouchableOpacity } from 'react-native'
-import { JSX } from 'react'
-
+import { JSX, useState } from 'react'
 import { Link, router } from 'expo-router'
 
 import Button from '../../components/Button'
@@ -13,12 +12,30 @@ const handlePress = (): void => {
 }
 
 const LogIn = (): JSX.Element => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <View style={styles.container}>
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
-                <TextInput style={styles.input} value="Email address" />
-                <TextInput style={styles.input} value="Password" />
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={(text) => { setEmail(text) }}
+                    autoCapitalize='none'
+                    // keyboardType='email-address'
+                    placeholder='Email Address'
+                    textContentType='emailAddress'
+                />
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={(text) => { setPassword(text) }}
+                    autoCapitalize='none'
+                    secureTextEntry
+                    placeholder='Password'
+                    textContentType='password'
+                />
                 <Button label="Submit" onPress={handlePress} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Not registered?</Text>
