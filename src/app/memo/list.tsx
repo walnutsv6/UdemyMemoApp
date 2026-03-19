c6693823ad52a7d3fe5e53c2d959219d56e4d516
Tomofumi Miyahara <miyahara@lyzon.co.jp>
2026-03-19 15:39:29 +0900

    update memolist

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,12 +1,15 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList } from 'react-native'
 import { JSX } from 'react'
 import { router, useNavigation } from 'expo-router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
 import LogOutButton from '../../components/LogOutButton'
+import { db, auth } from '../../config'
+import { type Memo } from '../../../types/memo'
 
 const handlePress = (): void => {
     // 新規メモ画面へ移動
@@ -14,6 +17,7 @@ const handlePress = (): void => {
 }
 
 const List = (): JSX.Element => {
+    const [memos, setMemos] = useState<Memo[]>([])
     const navigation = useNavigation()
     useEffect(() => {
         navigation.setOptions({
@@ -22,15 +26,34 @@ const List = (): JSX.Element => {
             }
         })
     }, [])
+    useEffect(() => {
+        if (auth.currentUser === null) { return }
+        const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+        const q = query(ref, orderBy('updatedAt', 'desc'))
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const remoteMemos: Memo[] = []
+            snapshot.forEach((doc) => {
+                console.log('memo', doc.data())
+                const { bodyText, updatedAt } = doc.data()
+                remoteMemos.push({
+                    id: doc.id,
+                    bodyText,
+                    updatedAt
+                })
+            })
+            setMemos(remoteMemos)
+        })
+        return unsubscribe
+    }, [])
     return (
         <View style={styles.container}>
             {/* ヘッダー */}
             {/* メモリスト */}
-            <View>
-                <MemoListItem />
-                <MemoListItem />
-                <MemoListItem />
-            </View>
+            <FlatList
+                data={memos}
+                renderItem={({ item }) => <MemoListItem memo={item} />}
+                keyExtractor={(item) => item.id}
+            />
 
             {/* 追加ボタン */}
             <CircleButton onPress={handlePress}>
