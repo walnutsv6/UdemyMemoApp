d505236e4c3befca7f6ff4e0ce3a2f9244fe1999
Tomofumi Miyahara <miyahara@lyzon.co.jp>
2026-03-19 11:47:31 +0900

    fix KeyboardAvoidingView iOS bug

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,8 +1,9 @@
-import { View, TextInput, StyleSheet, KeyboardAvoidingView } from 'react-native'
+import { View, TextInput, StyleSheet } from 'react-native'
 import { JSX, useState } from 'react'
 import { router } from 'expo-router'
 import { collection, addDoc, Timestamp } from 'firebase/firestore'
 
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
 import { db, auth } from '../../config'
@@ -28,9 +29,9 @@ const handlePress = (bodyText: string): void => {
 const Create = (): JSX.Element => {
     const [bodyText, setBodyText] = useState('')
     return (
-        <KeyboardAvoidingView style={styles.container} behavior='height'>
+        <KeyboardAvoidingView style={styles.container}>
             <View style={styles.InputContainer}>
-                <TextInput style={styles.input} value={bodyText} onChangeText={(text) => setBodyText(text)} multiline />
+                <TextInput style={styles.input} value={bodyText} onChangeText={(text) => setBodyText(text)} multiline autoFocus />
             </View>
             <CircleButton onPress={() => handlePress(bodyText)}>
                 <Icon name="check" size={40} color="#ffffff" />
